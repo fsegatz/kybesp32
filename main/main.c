ac06a3239b7ff31abf71e63d77424612e9aa5e8d
Fabian Segatz <fabian.segatz@gmail.com>
2022-09-15 17:11:11 +0200

    Simple FreeRTOS task

--- main/main.c
+++ main/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+const uint32_t print_task_stack = 1024;
+const uint32_t print_task_priority = configMAX_PRIORITIES;
+
+void print_task(void) {
+    while(1) {
+        printf("hallo welt\n");
+        fflush(stdout);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 void app_main(void)
 {
-    printf("hallo welt");
-    fflush(stdout);
+    xTaskCreatePinnedToCore(&print_task, "Print task", print_task_stack, NULL, print_task_priority, NULL, 0);
+    return;
 }
+
